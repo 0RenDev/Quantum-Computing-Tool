fd554908362e4923f7ab1810e8a5f09a9c84f639
RyanRajDev <82542223+0RenDev@users.noreply.github.com>
2024-02-29 02:18:54 -0800

    Update qiskit-ryan.py

--- QiskitExamples/qiskit-ryan.py
+++ QiskitExamples/qiskit-ryan.py
@@ -14,5 +14,5 @@ qc.cz(0, 1)
 # convert the final product into a state vector to be printed
 sv = qi.Statevector.from_instruction(qc)
 
-print('Final state vector : ', sv)
+print('Final state vector : ', str(sv.draw("text")))
 
