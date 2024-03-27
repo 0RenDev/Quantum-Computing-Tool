78be24c2079135d398dd3c26fb9d6208a85b85a7
Sean <123417097+slowe8@users.noreply.github.com>
2024-03-26 21:52:19 -0700

    Implemented Operator and Qbit classes and some of their functionality

---
+++ Quantum/Qbit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Xml.Schema;
+using LinearAlgebra;
+using Vector = LinearAlgebra.Vector;
+
+namespace Quantum
+{
+    public class Qbit(Complex[] stateVector)
+    {
+        private Vector data = new Vector(stateVector);
+
+        // Evolves a Quantum State Vector (Qbit) using an Operator
+        public void Evolve(Operator @operator)
+        {
+            Vector evolvedState = Operations.MatrixVectorMult(@operator.Data, this.data);
+
+            data = evolvedState;
+        }
+
+        // Override of to String
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+    }
+}
